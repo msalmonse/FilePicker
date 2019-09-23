15c37ced2f74bdce10f6ae793b3f7380dd139df2
Michael Salmon <msalmonse@gmail.com>
2019-09-23 08:53:08 +0200

    More public

--- Sources/FilePicker/FilePickerView.swift
+++ Sources/FilePicker/FilePickerView.swift
@@ -9,11 +9,15 @@ import Foundation
 import SwiftUI
 
 @available(iOS 13.0, *)
-struct FilePickerView: View {
+public struct FilePickerView: View {
     @ObservedObject
     var state: FilePickerState
     @Environment(\.presentationMode)
     var mode: Binding<PresentationMode>
+
+    public init(_ state: FilePickerState) {
+        self.state = state
+    }
 
     func doDismiss() {
         mode.wrappedValue.dismiss()
