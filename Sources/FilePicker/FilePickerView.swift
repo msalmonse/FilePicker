edf7c28c2d6f24b45674b96c57ac754e4987b70f
Michael Salmon <msalmonse@gmail.com>
2021-05-05 10:26:46 +0200

    Added toggles to view

--- Sources/FilePicker/FilePickerView.swift
+++ Sources/FilePicker/FilePickerView.swift
@@ -16,9 +16,11 @@ public struct FilePickerView: View {
     var state: FilePickerState
     @Environment(\.presentationMode)
     var mode: Binding<PresentationMode>
+    let toggles: Bool
 
-    public init(_ state: FilePickerState) {
+    public init(_ state: FilePickerState, toggles: Bool = false) {
         self.state = state
+        self.toggles = toggles
     }
 
     func doDismiss() {
@@ -27,5 +29,16 @@ public struct FilePickerView: View {
 
     public var body: some View {
         FilePickerController(state, { self.doDismiss() })
+        VStack {
+            FilePickerController(state, { self.doDismiss() })
+            if toggles {
+                HStack(spacing: 50) {
+                    Spacer(minLength: 50)
+                    Toggle("File Extensions?", isOn: $state.shouldShowFileExtensions)
+                    Toggle("Multi selections?", isOn: $state.allowsMultipleSelection)
+                    Spacer(minLength: 50)
+                }
+            }
+        }
     }
 }
